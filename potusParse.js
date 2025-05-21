f6a702c6b9686d6942daa9b75b388a55e317a7de
wlcsv <wlcsv8080@outlook.com>
2025-05-21 13:31:52 -0300

    Código atualizado para utilizar recursos mais modernos do ecossistema JS.

--- potusParse.js
+++ potusParse.js
@@ -1,32 +1,33 @@
-const rp = require('request-promise');
+const axios = require('axios');
 const cheerio = require('cheerio');
 
-const potusParse = (url) => {
-  return rp(url)
-    .then((html) => {
-      const $ = cheerio.load(html);
+const potusParse = async (url) => {
+  try {
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-      const name = $('tr > th > .fn').text();
+    const name = $('tr > th > div.fn[style="font-size:125%;"]').text();
 
-      const birthday = new Date(
-        $('th.infobox-label:contains("Born")')
-          .next('td')
-          .contents()
-          .filter((i, el) => el.type === 'text')[0]
-          .data
-          .trim()
-        )
-        .toISOString()
-        .split("T")[0];
+    const birthday = new Date(
+      $('th.infobox-label:contains("Born")')
+        .next('td')
+        .contents()
+        .filter((i, el) => el.type === 'text')[0]
+        .data
+        .trim()
+        .replace(/[^a-zA-Z0-9, ]/g, '')
+      )
+      .toISOString()
+      .split("T")[0];
 
-      return {
-        name: name,
-        birthday: birthday,
-      };
-    })
-    .catch((err) => {
+    return {
+      name: name,
+      birthday: birthday,
+    };
+  } catch(err) {
       throw new Error(err.message);
-    })
+  }
 };
 
 module.exports = potusParse;
--- potusScraper.js
+++ potusScraper.js
@@ -1,20 +1,41 @@
-const rp = require('request-promise');
+const axios = require('axios');
 const cheerio = require('cheerio');
+
+const potusParse = require('./potusParse');
+
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 
-rp(url)
-  .then((html) => {
+const presidents = async () => {
+  try {
     // success!
+    const response = await axios.get(url);
+    const html = response.data;
     const $ = cheerio.load(html)
+
     const wikiUrls = [];
     const quantityOfPresidents = $('td > b > a').length; 
+
     for (let i = 0; i < quantityOfPresidents; i++) {
       wikiUrls.push($('td > b > a', html)[i].attribs.href);
     }
-    console.log(wikiUrls);
-    console.log(quantityOfPresidents);
-  })    
-  .catch((err) => {
+
+    const results = await Promise.all(
+      wikiUrls.map((url) => {
+        let completeUrl = 'https://en.wikipedia.org' + url;
+        return potusParse(completeUrl);
+      })
+    ); 
+
+    return results;
+
+  } catch(err) {
     // handle error
-    console.log(err);
-  });
+    throw new Error(err.message);
+  };
+};
+
+// Function running
+(async () => {
+  const result = await presidents();
+  console.log(result);
+})();
